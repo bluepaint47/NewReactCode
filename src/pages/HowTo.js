e83aa3299ddeeefb6d9c955be5bf0da2ac7b8dd8
GF Blue <gfblue47@gmail.com>
2023-10-09 16:40:15 +0500

    changes

---
+++ src/pages/HowTo.js
@@ -0,0 +1,21 @@
+
+import Intro from "../components/howto/Intro";
+import Feat from "../components/home/Feat"
+import Use from "../components/home/Use"
+import Footer from "../components/home/Footer";
+
+const HowTo = () => {
+
+
+    return (
+        <>
+      <Intro></Intro>
+      <Feat></Feat>
+      <Use></Use>
+      <Footer></Footer>
+      
+        </>
+    )
+  };
+  
+  export default HowTo;
---
+++ src/pages/Products.js
@@ -0,0 +1,20 @@
+import Feature from "../components/home/Feature";
+import Pro from "../components/products/Pro"
+import Produc from "../components/products/produc"
+import News from "../components/home/News";
+import Footer from "../components/home/Footer";
+
+const Products = () => {
+    
+
+    return (
+        <>
+      <Pro></Pro>
+      <Produc></Produc>
+      <News></News>
+      <Footer></Footer>
+        </>
+    )
+  };
+  
+  export default Products;
